5b54b366808646a63cabd0734497da99e777902d
Ryan Brunner <ryan@ryanbrunner.com>
2012-12-03 21:14:08 -0500

    Fixing git

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -4,7 +4,7 @@ describe HerokuStatus::Git do
   describe "branches" do
     let(:input) { "buncha stuff from git" }
     it "should list branches" do
-      described_class.new(Dir.pwd).branches.should == {'master' => 'fdebdee'}
+      described_class.new(Dir.pwd).branches.should == {'master' => 'fca6114'}
     end
   end
 end
