a9fb14fb7cb5fae7c7d1f7da7883c4ac47768bb9
Ryan Brunner <ryan@ryanbrunner.com>
2012-12-03 20:41:48 -0500

    Test git

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe HerokuStatus::App do
+  let(:app_name) { "forzapanino2" }
+
   describe "commit" do
-    let(:app_name) { "forzapanino2" }
+
 
     it "should return the current commit" do
       VCR.use_cassette('heroku_release') do
@@ -14,6 +16,17 @@ describe HerokuStatus::App do
       VCR.use_cassette('heroku_release') do
         HerokuStatus::Git.any_instance.stub(:branches).and_return("master" => "d12cf91")
         HerokuStatus::App.get(app_name).current_branches.should include('master')
+      end
+    end
+  end
+
+  describe "compare_branches" do
+    it "should list differences between branches" do
+       VCR.use_cassette('heroku_release') do
+        HerokuStatus::Git.any_instance.stub(:branches).and_return("master" => "d12cf91")
+        HerokuStatus::App.get(app_name).compare_branches.should =~ [
+          { branch: 'master', ahead: 3, behind: 2 }
+        ]
       end
     end
   end
